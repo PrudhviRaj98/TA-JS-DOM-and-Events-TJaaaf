ef5293f011a268e147b4a181105fd40f164d9516
prudhvu raj <rprudhvi645@gmail.com>
2021-05-04 18:45:10 +0530

    refactored

--- block-BJaaek/code/script.js
+++ block-BJaaek/code/script.js
@@ -1,19 +1,23 @@
-let ban = document.querySelector(".banner");
+
 let first = document.querySelector(".first");
-ban.addEventListener('click' , function(){
-    let color = '#' + Math.random().toString(16).slice(2,8);
-    first.style.backgroundColor = color;
+first.addEventListener('click' , function(){
+    first.style.backgroundColor = generate();
     first.innerHTML = backgroundColor;
 });
 
-
-let banB = document.querySelector(".banner");
-let second = document.querySelector(".second")[1];
-banB.addEventListener('onmousemove' , function(){
+function generate (){
     let color = '#' + Math.random().toString(16).slice(2,8);
-    second.style.backgroundColor = color;
-    second.innerHTML = backgroundColor;
+    return color
+}
+
+let boxTwo = document.querySelector(".second");
+
+boxTwo.addEventListener('mousemove' , function(){
+    boxTwo.style.backgroundColor = generate();
+    boxTwo.innerHTML = backgroundColor;
 });
+
+
 
 
 
