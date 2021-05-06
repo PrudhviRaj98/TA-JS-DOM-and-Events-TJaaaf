92d912481adc39d8bcefc01971159d6412cd6dce
prudhvu raj <rprudhvi645@gmail.com>
2021-05-06 18:29:10 +0530

    assignment

---
+++ block-BJaaeo/code/script.js
@@ -0,0 +1,10 @@
+let boxes = document.querySelectorAll(".btn");
+let clear = document.querySelector(".clear");
+let equal = document.querySelector(".equal");
+
+boxes.forEach((ele) => {
+    ele.addEventListener("click", (event)=>{
+        let text = event.target.innerText;
+        
+    });
+});
